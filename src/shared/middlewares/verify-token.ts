cf5729e44f9a04cc7d504968f3936c75fbdb17fd
Luan Henrique <luanhenriquemiguelalves@gmail.com>
2026-05-19 00:28:23 -0300

    feat: creating verify token middleware

---
+++ src/shared/middlewares/verify-token.ts
@@ -0,0 +1,11 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { UnauthorizedError } from "../errors/domain-errors.js";
+
+export async function verifyToken(req: FastifyRequest, reply: FastifyReply) {
+    try {
+        await req.jwtVerify()
+    }
+    catch(error){
+        throw new UnauthorizedError("Token inválido")
+    }
+}
