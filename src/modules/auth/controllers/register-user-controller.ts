f440856c686f2b56da0fdefa0a90d7893bad7ff9
Luan Henrique <luanhenriquemiguelalves@gmail.com>
2026-05-19 23:43:52 -0300

    refactor: using token service in register controller

--- src/modules/auth/controllers/register-user-controller.ts
+++ src/modules/auth/controllers/register-user-controller.ts
@@ -1,23 +1,29 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { registerUserSchema } from "../schemas/register-user-schema.js";
 import { RegisterUserService } from "../services/register-user-service.js";
+import { TokenService } from "../services/token-service.js";
 
 export class RegisterUserController {
-    constructor(private registerUserService: RegisterUserService) { }
+    constructor(private registerUserService: RegisterUserService, private tokenService: TokenService) { }
 
     async handle(req: FastifyRequest, reply: FastifyReply) {
         const body = registerUserSchema.parse(req.body)
         const user = await this.registerUserService.execute(body)
 
-        const token = await reply.jwtSign({
-            userId: user.id
-        }, {
-            sign: { expiresIn: "15min" }
+        const { accessToken, refreshToken } = await this.tokenService.generateTokens({ userId: user.id, fastify: req.server })
+
+        reply.setCookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            path: '/auth/refresh',
+            maxAge: 60 * 60 * 24 * 7
         })
 
-        return reply.status(201).send({
+        return reply.status(200).send({
             message: "Usuário cadastrado com sucesso",
-            token,
+            accessToken,
+            user
         })
     }
 }
--- src/modules/auth/factories/make-register-controller.ts
+++ src/modules/auth/factories/make-register-controller.ts
@@ -1,11 +1,15 @@
 import { RegisterUserController } from "../controllers/register-user-controller.js"
+import { RefreshTokenRepository } from "../repositories/refresh-token-repository.js"
 import { SessionRepository } from "../repositories/session-repository.js"
 import { RegisterUserService } from "../services/register-user-service.js"
+import { TokenService } from "../services/token-service.js"
 
 export function makeRegisterController() {
   const sessionRepository = new SessionRepository()
   const registerUserService = new RegisterUserService(sessionRepository)
-  const registerUserController = new RegisterUserController(registerUserService)
+  const refreshTokenRepository = new RefreshTokenRepository()
+  const tokenService = new TokenService(refreshTokenRepository)
+  const registerUserController = new RegisterUserController(registerUserService, tokenService)
 
   return registerUserController
 }
